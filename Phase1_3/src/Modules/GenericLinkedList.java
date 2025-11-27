3f41fd80018654cf68cf122740c9cd2026ef1fe9
amirmohammadhamidi <amirmohammadhmd.20@gmail.com>
2025-11-28 00:36:09 +0330

    packagin complete

---
+++ Phase1_3/src/Modules/GenericLinkedList.java
@@ -0,0 +1,141 @@
+package Modules;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+class GenericNode<T> {
+    private T data;
+    private GenericNode<T> nextNode;
+    GenericNode(T data , GenericNode<T> nextNode){
+        this.data = data;
+        this.nextNode = nextNode;
+    }
+
+    public T getData(){
+        return data;
+    }
+
+    public GenericNode<T> getNextNode(){
+        return nextNode;
+    }
+
+    public void setData(T data){
+        this.data = data;
+    }
+
+    public void setNextNode(GenericNode<T> nextNode){
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString(){
+        return data.toString();
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if(object == null || object.getClass() != getClass()) return false;
+        else if(object == this) return true;
+        else {
+            GenericNode<?> node = (GenericNode<?>) object;
+            return Objects.equals(node.getData() , data) &&
+                    Objects.equals(node.getNextNode() , nextNode);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data , nextNode);
+    }
+}
+
+public class GenericLinkedList<T> {
+    private GenericNode<T> headNode;
+    private int size;
+
+    public GenericLinkedList(){
+        headNode = null;
+        size = 0;
+    }
+
+    public void insert(T data){
+        if(isEmpty()){
+            headNode = new GenericNode<>(data , null);
+            incrementSize();
+        }
+        else {
+            GenericNode<T> newNode = new GenericNode<>(data , null);
+            findTailNode().setNextNode(newNode);
+        }
+    }
+
+    public void removeByKey(T key){
+        if(isEmpty()) return;
+
+        // Remove invalid heads
+        while(headNode != null && headNode.getData().equals(key)){
+            headNode = headNode.getNextNode();
+            decrementSize();
+        }
+
+        // Remove invalid nodes
+        GenericNode<T> prevNode = headNode;
+        GenericNode<T> node = headNode.getNextNode();
+
+        while(node != null){
+            if(node.getData().equals(key)){
+                node = node.getNextNode();
+                prevNode.setNextNode(node);
+                decrementSize();
+            }
+            else {
+                prevNode = node;
+                node = node.getNextNode();
+            }
+        }
+
+    }
+
+    public void printList(){
+        GenericNode<T> node = headNode;
+        while(node != null){
+            System.out.println(node);
+            node = node.getNextNode();
+        }
+    }
+
+    private void incrementSize(){
+        size += 1;
+    }
+
+    private void decrementSize(){
+        if(size >= 1) size -= 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public GenericNode<T> getHeadNode(){
+        return headNode;
+    }
+
+    private GenericNode<T> findTailNode(){
+        GenericNode<T> node = headNode;
+        while(node.getNextNode() != null){
+            node = node.getNextNode();
+        }
+        return node;
+    }
+
+    public ArrayList<T> getKeysArrayList(){
+        ArrayList<T> keysArrayList = new ArrayList<>();
+        GenericNode<T> node = headNode;
+        while(node != null){
+            keysArrayList.add(node.getData());
+            node = node.getNextNode();
+        }
+        return keysArrayList;
+    }
+}
+
---
+++ Phase1_3/src/Modules/Library.java
@@ -0,0 +1,61 @@
+package Modules;
+
+import Common.Status;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+public class Library {
+    private GenericLinkedList<Book> books;
+
+    public Library(){
+        books = new GenericLinkedList<>();
+    }
+
+    public void insertBook(Book book){
+        books.insert(book);
+    }
+
+    public void removeBook(Book book){
+        books.removeByKey(book);
+    }
+
+    public void updateBook(Book book , Status status){
+        GenericNode<Book> node = books.getHeadNode();
+        while(node != null){
+            if(node.getData().equals(book)){
+                node.getData().setStatus(status);
+                break;
+            }else node = node.getNextNode();
+        }
+    }
+
+    public void printBooksList(){
+        books.printList();
+    }
+
+    public Book searchBooksByTitle(String title){
+        GenericNode<Book> node = books.getHeadNode();
+        while(node != null){
+            if(node.getData().getTitle().equals(title)) return node.getData();
+            else node = node.getNextNode();
+        }
+        return null;
+    }
+
+    public ArrayList<Book> searchBooksByAuthor(String author){
+        ArrayList<Book> authorBooks = new ArrayList<>();
+        GenericNode<Book> node = books.getHeadNode();
+        while(node != null){
+            if(node.getData().getAuthor().equals(author)) authorBooks.add(node.getData());
+            node = node.getNextNode();
+        }
+        return authorBooks;
+    }
+
+    public ArrayList<Book> sortBooksByReleaseYear(){
+        ArrayList<Book> sortedBooks = books.getKeysArrayList();
+        sortedBooks.sort(Comparator.comparingInt(Book::getReleaseYear));
+        return sortedBooks;
+    }
+}
