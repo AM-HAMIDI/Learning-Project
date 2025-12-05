bde1d845bb036899de3cc6017b77935108f23e39
amirmohammadhamidi <amirmohammadhmd.20@gmail.com>
2025-12-06 00:58:51 +0330

    write Generic linked list

---
+++ Phase1/src/GenericLinkedList.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class GenericLinkedList<T> {
+    private GenericNode<T> headNode;
+    private int size;
+
+    public GenericLinkedList(){
+        headNode = null;
+        size = 0;
+    }
+
+    public void insert(T data){
+        if(isEmpty()){
+            headNode = new GenericNode<>(data , null);
+            incrementSize();
+        }
+        else {
+            GenericNode<T> newNode = new GenericNode<>(data , null);
+            findTailNode().setNextNode(newNode);
+        }
+    }
+
+    public void removeByKey(T key){
+        if(isEmpty()) return;
+
+        while(headNode != null && headNode.getData().equals(key)){
+            headNode = headNode.getNextNode();
+            decrementSize();
+        }
+
+        GenericNode<T> prevNode = headNode;
+        GenericNode<T> node = headNode.getNextNode();
+
+        while(node != null){
+            if(node.getData().equals(key)){
+                node = node.getNextNode();
+                prevNode.setNextNode(node);
+                decrementSize();
+            }
+            else {
+                prevNode = node;
+                node = node.getNextNode();
+            }
+        }
+
+    }
+
+    public void printList(){
+        GenericNode<T> node = headNode;
+        while(node != null){
+            System.out.println(node);
+            node = node.getNextNode();
+        }
+    }
+
+    public GenericNode<T> getHeadNode(){
+        return headNode;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void incrementSize(){
+        size += 1;
+    }
+
+    private void decrementSize(){
+        if(size >= 1) size -= 1;
+    }
+
+    private GenericNode<T> findTailNode(){
+        GenericNode<T> node = headNode;
+        while(node.getNextNode() != null){
+            node = node.getNextNode();
+        }
+        return node;
+    }
+
+    public ArrayList<T> getKeysArrayList(){
+        ArrayList<T> keysArrayList = new ArrayList<>();
+        GenericNode<T> node = headNode;
+        while(node != null){
+            keysArrayList.add(node.getData());
+            node = node.getNextNode();
+        }
+        return keysArrayList;
+    }
+}
+
