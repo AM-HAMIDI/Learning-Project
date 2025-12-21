1e09ca065e57210a7e257271d77eb7682beb078c
amirmohammadhamidi <amirmohammadhmd.20@gmail.com>
2025-12-22 01:38:39 +0330

    Finish Phase2.2

--- src/main/java/com/mahsan/library/Main.java
+++ src/main/java/com/mahsan/library/Main.java
@@ -3,11 +3,9 @@ package com.mahsan.library;
 import com.mahsan.library.app.AppManager;
 
 public class Main {
-    public static final String projectRootPath = System.getProperty("user.dir");
-
     public static void main(String[] args) {
-        AppManager systemManager = new AppManager();
-        systemManager.initializeSystem();
-        systemManager.startSystem();
+        AppManager appManager = new AppManager();
+        appManager.initializeSystem();
+        appManager.startSystem();
     }
 }
